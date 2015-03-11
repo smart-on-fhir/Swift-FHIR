9c20882f2cee9338855e01649474946a8d87361b
Pascal Pfiffner <phase.of.matter@gmail.com>
2015-03-11 12:27:20 +0100

    Submodule update for better FHIRServer handling

--- Models/DomainResource.swift
+++ Models/DomainResource.swift
@@ -20,24 +20,12 @@ public class DomainResource: Resource
 		get { return "DomainResource" }
 	}
 	
-	/// Additional Content defined by implementations
-	public var extension_fhir: [Extension]?
-	
-	/// Extensions that cannot be ignored
-	public var modifierExtension: [Extension]?
-	
 	/// Text summary of the resource, for human interpretation
 	public var text: Narrative?
 	
 	public required init(json: JSONDictionary?) {
 		super.init(json: json)
 		if let js = json {
-			if let val = js["extension"] as? [JSONDictionary] {
-				self.extension_fhir = Extension.from(val, owner: self) as? [Extension]
-			}
-			if let val = js["modifierExtension"] as? [JSONDictionary] {
-				self.modifierExtension = Extension.from(val, owner: self) as? [Extension]
-			}
 			if let val = js["text"] as? JSONDictionary {
 				self.text = Narrative(json: val, owner: self)
 			}
@@ -47,12 +35,6 @@ public class DomainResource: Resource
 	override public func asJSON() -> JSONDictionary {
 		var json = super.asJSON()
 		
-		if let extension_fhir = self.extension_fhir {
-			json["extension"] = Extension.asJSONArray(extension_fhir)
-		}
-		if let modifierExtension = self.modifierExtension {
-			json["modifierExtension"] = Extension.asJSONArray(modifierExtension)
-		}
 		if let text = self.text {
 			json["text"] = text.asJSON()
 		}

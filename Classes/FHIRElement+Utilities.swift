349544939a54ef6a309bff068f33b0a34e6ec673
Pascal Pfiffner <phase.of.matter@gmail.com>
2015-08-14 18:03:35 +0200

    Add instantiation utility methods

--- Classes/FHIRElement+Utilities.swift
+++ Classes/FHIRElement+Utilities.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 SMART Health IT. All rights reserved.
 //
 
+import Foundation
+
 
 public extension FHIRElement
 {
@@ -14,3 +16,44 @@ public extension FHIRElement
 		return extension_fhir?.filter() { return $0.url?.absoluteString == uri }
 	}
 }
+
+
+public extension FHIRResource
+{
+	/**
+	Attempt to instantiate a Resource of the receiving class by reading a JSON file at the given filesystem path.
+	
+	:param path: The local path to read the JSON file from
+	:param error: An NSError pointer to fill on failure
+	:returns: An instance of the receiving class or nil
+	*/
+	final class func instantiateFromPath(path: String, error: NSErrorPointer) -> Self? {
+		if let data = NSData(contentsOfFile: path, options: nil, error: error) {
+			if let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: error) as? FHIRJSON {
+				return self.init(json: json)
+			}
+		}
+		return nil
+	}
+}
+
+
+extension NSBundle
+{
+	/**
+	Attempt to read a JSON file with the given name (.json) from the bundle, parse the JSON and instantiate a FHIR resource corresponding
+	to the "resourceType" in the file.
+	
+	:returns: A FHIRResource subclass corresponding to the "resourceType" entry or nil
+	*/
+	public func fhir_bundledResource(name: String) -> FHIRResource? {
+		if let url = URLForResource(name, withExtension: "json"),
+			let data = NSData(contentsOfURL: url),
+			let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? FHIRJSON {
+				return FHIRElement.instantiateFrom(json, owner: nil) as? FHIRResource
+		}
+		fhir_logIfDebug("Failed to read bundled resource named «\(name)»")
+		return nil
+	}
+}
+

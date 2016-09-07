46c1537500870d2370346c2d85accad4c1189b2f
Pascal Pfiffner <phase.of.matter@gmail.com>
2016-09-07 14:26:41 +0200

    Add Patient extension for goodies

---
+++ Sources/Client/Patient+SMART.swift
@@ -0,0 +1,86 @@
+//
+//  Patient+SMART.swift
+//  SwiftFHIR
+//
+//  Created by Pascal Pfiffner on 07.09.16.
+//  2016, SMART Health IT.
+//
+
+import Foundation
+#if !NO_MODEL_IMPORT
+import Models
+#endif
+
+
+extension Patient {
+	
+	/// Easy way to retrieve a string for the patient's name, with a preference for the "usual" use name.
+	public var humanName: String? {
+		guard let names = name else {
+			return nil
+		}
+		var useName: HumanName?
+		for name in names {
+			if nil == useName || "usual" == name.use {
+				useName = name
+			}
+			else if let use = name.use, "usual" == use || "official" == use {
+				useName = name
+			}
+		}
+		return useName?.human
+	}
+	
+	/// Uses the system's date formatter to format the birthdate as a short date.
+	public var humanBirthDateShort: String? {
+		guard let birthdate = birthDate else {
+			return nil
+		}
+		let formatter = DateFormatter()
+		formatter.dateStyle = .short
+		formatter.timeStyle = .none
+		return formatter.string(from: birthdate.nsDate)
+	}
+	
+	/// Uses the system's date formatter to format the birthdate as a medium date.
+	public var humanBirthDateMedium: String? {
+		guard let birthdate = birthDate else {
+			return nil
+		}
+		let formatter = DateFormatter()
+		formatter.dateStyle = .medium
+		formatter.timeStyle = .none
+		return formatter.string(from: birthdate.nsDate)
+	}
+}
+
+
+extension HumanName {
+	
+	/// Join the non-empty name parts into a "human-normal" string in the order prefix > given > family > suffix, joined by a space,
+	/// **unless** the receiver's `text` is set, in which case the text is returned.
+	public var human: String? {
+		if let text = text {
+			return text
+		}
+		
+		var parts = [String]()
+		if let prefix = prefix {
+			parts.append(contentsOf: prefix.filter() { $0.characters.count > 0 })
+		}
+		if let given = given {
+			parts.append(contentsOf: given.filter() { $0.characters.count > 0 })
+		}
+		if let family = family {
+			parts.append(contentsOf: family.filter() { $0.characters.count > 0 })
+		}
+		if let suffix = suffix {
+			parts.append(contentsOf: suffix.filter() { $0.characters.count > 0 })
+		}
+		guard parts.count > 0 else {
+			return nil
+		}
+		return parts.joined(separator: " ")
+	}
+}
+

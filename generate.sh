18d2ebec565d00671d9d411d295dbf552dc18644
Amit Jain <gravicle@gmail.com>
2017-04-18 16:12:08 -0700

    Install requirements in generate script

--- generate.sh
+++ generate.sh
@@ -6,9 +6,12 @@ if [ ! -e fhir-parser ]; then
 	git submodule update --init
 fi
 
+pip3 install -r generator-requirements.txt
+
 # always copy our mappings and settings, then run the generator
 cp fhir-parser-resources/mappings.py fhir-parser/mappings.py
 cp fhir-parser-resources/settings.py fhir-parser/settings.py
 cd fhir-parser
+pip3 install -r requirements.txt
 ./generate.py $1 $2
 cd ..
